6b09da10eb38f5fdd0805b329001e61fccbc5114
up9cloud <8325632+up9cloud@users.noreply.github.com>
2023-02-21 09:31:09 +0000

    arm64

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM ekidd/rust-musl-builder as builder
+FROM --platform=linux/amd64 messense/rust-musl-cross:x86_64-musl as builder-amd64
 
 # We need to add the source code to the image because `rust-musl-builder`
 # assumes a UID of 1000, but TravisCI has switched to 2000.
@@ -6,7 +6,18 @@ ADD . ./
 RUN sudo chown -R rust:rust .
 RUN cargo build --release
 
-FROM scratch
-COPY --from=builder /home/rust/src/target/x86_64-unknown-linux-musl/release/echo-server /app/main
+FROM --platform=linux/amd64 scratch
+COPY --from=builder-amd64 /home/rust/src/target/x86_64-unknown-linux-musl/release/echo-server /app/main
+WORKDIR /app
+CMD ["./main"]
+
+FROM --platform=linux/arm64 messense/rust-musl-cross:aarch64-musl as builder-arm64
+
+ADD . ./
+RUN sudo chown -R rust:rust .
+RUN cargo build --release
+
+FROM --platform=linux/arm64 scratch
+COPY --from=builder-arm64 /home/rust/src/target/aarch64-unknown-linux-musl/release/echo-server /app/main
 WORKDIR /app
 CMD ["./main"]
--- build/build-release.sh
+++ build/build-release.sh
@@ -1,30 +1,29 @@
 #!/bin/bash
 
-# from https://github.com/emk/rust-musl-builder/blob/master/examples/build-release
-
 set -euo pipefail
 
+URL=https://github.com/rust-cross/rust-musl-cross
 USER=$1
 NAME=$2
 LABEL=$3
 
-case `uname -s` in
-	Linux)
-		echo "Building static binaries using ekidd/rust-musl-builder"
-		image="$USER/$NAME"
-		docker build -t "$image" .
-		docker run -d --name c-"$NAME" "$image"
-		docker stop c-"$NAME"
-		docker cp c-"$NAME":/app/main "$NAME"
-		docker rm c-"$NAME"
-		# docker rmi "$image"
-		zip "$NAME"-"$LABEL".zip "$NAME"
-		ls -al
-		;;
-	*)
-		echo "Building standard release binaries"
-		cargo build --release
-		zip -j "$NAME"-"$LABEL".zip target/release/"$NAME"
-		ls -al
-		;;
+case $(uname -s) in
+Linux)
+	echo "Building static binaries using $URL"
+	image="$USER/$NAME"
+	docker build -t "$image" .
+	docker run -d --name c-"$NAME" "$image"
+	docker stop c-"$NAME"
+	docker cp c-"$NAME":/app/main "$NAME"
+	docker rm c-"$NAME"
+	# docker rmi "$image"
+	zip "$NAME"-"$LABEL".zip "$NAME"
+	ls -al
+	;;
+*)
+	echo "Building standard release binaries"
+	cargo build --release
+	zip -j "$NAME"-"$LABEL".zip target/release/"$NAME"
+	ls -al
+	;;
 esac
